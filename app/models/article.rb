45caad44899a112d0f6f75582cfbd2aa28938a2c
Bang Nguyen and Brian Grant <bang+brian@>
2016-09-07 10:53:39 -0700

    create one to many relationship with an article having many comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
   validates :title, :link, :user, :points, :presence => true
+  has_many :comments
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   validates :text, :user, :presence => true
+  belongs_to :article
 end
